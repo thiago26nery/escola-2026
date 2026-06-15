ca493ea295156d325f370b09040fea3871b49e1a
Thiago José Nery Oliveira <thiagojoseneryoliveira@gmail.com>
2026-06-15 18:59:02 -0300

    criando o projeto

---
+++ src/main/java/br/edu/ifmg/escola_2026/Escola2026Application.java
@@ -0,0 +1,13 @@
+package br.edu.ifmg.escola_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Escola2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Escola2026Application.class, args);
+	}
+
+}
